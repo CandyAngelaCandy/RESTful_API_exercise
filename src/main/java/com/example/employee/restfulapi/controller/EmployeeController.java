3e3e8a262e5f5d76026e462e151166aca913ce56
Candy <229355694@qq.com>
2018-05-05 20:20:25 +0800

    use flyway migrate data

--- src/main/java/com/example/employee/restfulapi/controller/EmployeeController.java
+++ src/main/java/com/example/employee/restfulapi/controller/EmployeeController.java
@@ -1,5 +1,19 @@
 package com.example.employee.restfulapi.controller;
 
+import com.example.employee.restfulapi.entity.Employee;
+import com.example.employee.restfulapi.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/employees")
 public class EmployeeController {
     //在此处完成Employee API
+
+
 }
--- src/main/java/com/example/employee/restfulapi/repository/EmployeeRepository.java
+++ src/main/java/com/example/employee/restfulapi/repository/EmployeeRepository.java
@@ -2,8 +2,22 @@ package com.example.employee.restfulapi.repository;
 
 import com.example.employee.restfulapi.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    List<Employee> getAllEmployees();
+
+    Employee getEmployee(int id);
+
+
+    @Modifying
+    Employee updateEmployee(Employee employee);
+
+
+    void deleteByName(String name);
 }
